5214c3632eb96509b9948d028db6d5910cebc6f6
lucas41 <delfini.lucas@hotmail.com>
2018-06-13 22:13:09 -0300

    so mudei o nome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 
 <h1><center> INICIO <center><h1>
 <br>
-<h3> bem vindo ao nosso veterinário <h3>
+<h3> bem vindo a nossa clínica veterinária <h3>
 
 <br><br>
 <a class="btn btn-primary" href="http://127.0.0.1:8000/caes"> Cachorros </a> &nbsp;  &nbsp;  &nbsp;  <a class="btn btn-primary" href="http://127.0.0.1:8000/caes/formulario"> Cadastro </a>
